b98668b47036f6e92a52bcd6f006e1ac1b0f10be
chptiger <thomas.java863@gmail.com>
2018-07-16 14:30:55 -0700

    String remove leading/trailing spaces -> two block

---
+++ leetcode/src/basicQuestions/String/removeLeadingTrailingSpace.java
@@ -0,0 +1,40 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+/**
+ * remove all leading/trailing and duplicate empty spaces(only have one empty space if duplicated spaces happen) from the input string
+ * 
+ * 
+ * _ _ _abc _ _ ed_ _ ef_ _
+ * abc_ed_ef
+ * @author eng_1
+ *
+ */
+public class removeLeadingTrailingSpace {
+	public static void main(String[] args){
+	
+		System.out.println(removeLeadingTrailingSpace("___abc__ed__ef__".toCharArray()));
+	}
+		
+	public static String removeLeadingTrailingSpace(char[] input) {
+		int slow = 0, fast = 0, count = 0;
+		 
+		while (true) {
+			// leading/trailing spaces
+			while(fast < input.length && input[fast] == '_') {
+				fast++;
+			}
+			if(fast == input.length) {
+				break;
+			}
+			// middle spaces
+			if(count > 0) {
+				input[slow++] = '_';
+			}
+			while(fast < input.length && input[fast] != '_') {
+				input[slow++] = input[fast++];
+			}
+			count++;
+		}
+		return new String(input).substring(0, slow - 1);
+	}
+}
