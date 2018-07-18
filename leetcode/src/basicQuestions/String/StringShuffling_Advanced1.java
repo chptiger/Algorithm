aeb45f4849cf392a76389e31380d478d3f2474df
chptiger <thomas.java863@gmail.com>
2018-07-18 14:14:37 -0700

    String advanced topic 1

--- leetcode/src/basicQuestions/String/StringShuffling_Advanced1.java
+++ leetcode/src/basicQuestions/String/StringShuffling_Advanced1.java
@@ -1,5 +1,3 @@
-package basicQuestions.String;
-
 /**
  * Shuffling 
  * 
@@ -16,28 +14,28 @@ public class StringShuffling_Advanced1 {
 
 	public static void main(String[] args) {
 		String input = "ABCDEFG1234567";
-		stringShuffling(input, 0, input.length() - 1);
+		input = stringShuffling(input.toCharArray(), 0, input.length() - 1);
 		System.out.println(input);
 	}
 
-	private static void stringShuffling(String input, int left, int right) {
+	private static String stringShuffling(char[] tmp, int left, int right) {
 		if ((right - left) <= 1) {
-			return;
+			return new String(tmp);
 		}
 
-		int size = input.length();
+		int size = right - left + 1; // used passed parameter. the size need to be caculated every time
 		int mid = left + size / 2;
 		int lm = left + size / 4;
 		int rm = left + (size * 3) / 4;
 
-		char[] tmp = input.toCharArray();
 		// I love yahoo -> yahoo love I
-		reverse(tmp, lm, rm - 1);
 		reverse(tmp, lm, mid - 1);
 		reverse(tmp, mid, rm - 1);
+		reverse(tmp, lm, rm - 1);
 
-		stringShuffling(input, left, left + 2 * (lm - left) - 1);
-		stringShuffling(input, left + 2 * (lm - left), right);
+		stringShuffling(tmp, left, left + 2 * (lm - left) - 1);
+		stringShuffling(tmp, left + 2 * (lm - left), right);
+		return new String(tmp);
 	}
 
 	public static void reverse(char[] input, int start, int end) {
