240e5e2365fd7f6907ab8738f6bf55c5bbc5575e
xx65 <thomas.java863@gmail.com>
2018-07-21 16:56:24 -0700

    String advance t0pic 4.2 find all anagram substring of longer strinbg

---
+++ leetcode/src/basicQuestions/String/allAnagramSubstringLongString_Advanced4_2.java
@@ -0,0 +1,39 @@
+package basicQuestions.String;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import utilities.ConstantsUtil;
+
+/**
+Description
+    Find all anagrams of a substring S2 in a long String S1
+ String s2 = "aabc";
+ String s1 = "zzzz cdeb caa b cyywwww";
+
+anagrams
+Anagrams don't necessarily have to use all the letters, but they normally do.
+"rail safety" = "fairy tales"
+"roast beef" = "eat for BSE"
+
+An anagram which means the opposite of its subject is called an "antigram". For example:
+"restful" = "fluster"
+"funeral" = "real fun"
+"adultery" = "true lady"
+
+ * https://docs.google.com/document/d/1Ic2DZ6fVZcbz9Y-OcEhc9-UUflLOvMcn5p4TOcGZO7Y/edit 
+ * @author xx65
+ *
+ */
+public class allAnagramSubstringLongString_Advanced4_2 {
+
+	@Test
+	public void test_UniqueSubstring() {
+		
+	}
+
+	
+}
