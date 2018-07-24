a54558a84324ccc1b36516c184d5771f92256c97
xx65 <thomas.java863@gmail.com>
2018-07-23 23:47:08 -0700

    String advanced topic 4.3 two pointers & sliding windows

--- leetcode/src/basicQuestions/String/LongestSubarrayContains1_Advanced4_3.java
+++ leetcode/src/basicQuestions/String/LongestSubarrayContains1_Advanced4_3.java
@@ -24,32 +24,45 @@ public class LongestSubarrayContains1_Advanced4_3 {
 		Assert.assertEquals(ConstantsUtil.FAIL, 8, longestSubarray(input.toCharArray(), k));
 	}
 
+//	keep there are max k 0 in the slide window.
 	private static int longestSubarray(char[] input, int k) {
-		if(input == null || input.length < k) {
+		if (input == null || input.length < k) {
 			return -1;
 		}
-		int result = 0, slow = 0, fast = 0, count = k;
-		while(fast < input.length) {
-//			 right-hand side
-				if(input[fast] == 0) {
-					if(count > 0) {// still inside the sliding window, we only consider the right-hand side of the sliding window
-						count--;
-						result++;
-					} else { // need to move left-hand side of the sliding window
-						if(input[slow] == 0) {
-							result--;
-							count++;
-						} else {
-							result--;
-						}
-					}
-				} else {
-					result++;
-				}
-//			result = Math.max(result, arg1);
-//			left-hand side
+		int result = 0, slow = 0, fast = 0, max = 0;
+		
+		/*there are two cases for right-hand side and left-hand side.
+		 * fast pointer:
+		 * 
+		 * if (input[fast] == '0') {  // next is 0
+				k--;
+				result++;
+			} else {				// next is 1
+				result++;
+			}
 			fast++;
+			result++;
+			
+			merge the code to make it more concise.
+		*/
+		while (fast < input.length) {
+			// right-hand side. add new 1 or 0 and count it.
+			if (input[fast] == '0') {
+				k--;
+			} 
+			fast++;
+			result++;
+			
+			// left-hand side. need to move, until removing the last 0
+			while (k < 0) { // the index of slow could be 1 or 0, we need keep moving until removing last 0, to keep the fixed 0 length in the sliding window
+				if(input[slow] == '0') {
+					k++;
+				} 
+				result--;
+				slow++;
+			}
+			max = Math.max(max, result);
 		}
-		return result;
+		return max;
 	}
 }
