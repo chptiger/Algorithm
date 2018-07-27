77544e4e6b722d287a5f3cd9e8f5752422fcd7ce
xx65 <thomas.java863@gmail.com>
2018-07-27 01:44:46 -0700

    Recursion & LinkedList

--- leetcode/src/basicQuestions/Recursion/II/ReverseLinkedListInPairs_66.java
+++ leetcode/src/basicQuestions/Recursion/II/ReverseLinkedListInPairs_66.java
@@ -21,12 +21,24 @@ public class ReverseLinkedListInPairs_66 {
 	public static ListNode a1 = new ListNode(1);
 	public static ListNode a2 = new ListNode(2);
 	public static ListNode a3 = new ListNode(3);
+	public static ListNode a4 = new ListNode(4);
 	
 	@Test
 	public void test_reverseLinkedListInPairs() {
 		a1.next = a2;
 		a2.next = a3;
+		a3.next = a4;
 		
-		Assert.equ
+		reverseLinkedListInPairs(a1);
+	}
+	
+	public static ListNode reverseLinkedListInPairs(ListNode head) {
+		if(head == null || head.next == null) {
+			return head;
+		}
+		ListNode tmp = head.next;
+		head.next = reverseLinkedListInPairs(head.next.next);
+		tmp.next = head;
+		return tmp;
 	}
 }
