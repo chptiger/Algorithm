13452a387c3e7d4ff2d161745758788ada821c35
Thomas <thomas@upnest.com>
2018-08-04 21:54:27 -0700

    udpated

--- leetcode/src/basicQuestions/Recursion/II/SpiralOrderTravel_2_4.java
+++ leetcode/src/basicQuestions/Recursion/II/SpiralOrderTravel_2_4.java
@@ -1,6 +1,6 @@
 package basicQuestions.Recursion.II;
 /**
- * /*Description
+ *Description
         Traverse an M  N 2D array in spiral order clock-wise starting from the top left corner. Return the list of traversal sequence.
         Assumptions
         The 2D array is not null and has size of M  N where M, N >= 0
@@ -10,7 +10,7 @@ package basicQuestions.Recursion.II;
         {9, 10, 11, 12} }
         the traversal sequence is [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]
         Medium
-        2 D Array*/
+        2 D Array
  * @author thomas
  *
  */
