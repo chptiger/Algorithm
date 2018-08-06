571e53b7de94068e467c0083958528d7e958f1b2
Thomas <thomas@upnest.com>
2018-08-05 19:02:43 -0700

    TODO: recursion with void method signature

--- leetcode/src/basicQuestions/Recursion/II/LowestCommonAncestorI_5_1.java
+++ leetcode/src/basicQuestions/Recursion/II/LowestCommonAncestorI_5_1.java
@@ -35,27 +35,42 @@ public class LowestCommonAncestorI_5_1 {
 	@Test
 	public void test_lowestCommonAncestor() {
 		TreeNode root = TreeUtil.createTree();
-		Assert.assertEquals(9, findLowestCommonAncestor(root, root.left.left, root.left.right).key);
+		TreeNode result = new TreeNode(100);
+		Assert.assertEquals(9, findLowestCommonAncestor(root, root.left.left, root.left.right, result).key);
 	}
 	
 	/**
 	 * 
 	 * After we find the lowest common ancestor TreeNode 9, the function still need to run all other nodes.
 	 * We do not need to traverse all the nodes, if we find the first ancestor, we should stop and return.
-	 * The question is how to stop the recursion function.
-	 * TODO Could we stop it in time?
+	 * The question is how to stop the current recursive method?
+	 * Answer: NO
+	 * Every recursion level is a new stack value to save current level variables. We could not pass the current level variable to other levels.
 	 * 
+	 * Wrong Answer.
+	 * 
+	 * If we use return method signature in the recursion method, we could get the updated value from return.
+	 * 
+	 * Question:
+	 * How the void recursion method to update the parameter?
+	 * 
+	 * TODO try to understand the recursion method with void.
+	 * To check if is it right.
+	 * 
+	 * TODO Could we stop it in time?
+	 * We could not. We use recursion method to get the bottom of the method, then backtracking from bottom to top level.
 	 */
-	public static TreeNode findLowestCommonAncestor(TreeNode root, TreeNode left, TreeNode right) {
+	public static TreeNode findLowestCommonAncestor(TreeNode root, TreeNode left, TreeNode right, TreeNode result) {
 		
-		if(root == null || left == root || right == root) {
+		if(root == null || left == root || right == root || result.key != 100) {
 			return root;
 		}
-		
-		TreeNode findLeft = findLowestCommonAncestor(root.left, left, right);
-		TreeNode findRight = findLowestCommonAncestor(root.right, left, right);
+		System.out.println(result.key);
+		TreeNode findLeft = findLowestCommonAncestor(root.left, left, right, result);
+		TreeNode findRight = findLowestCommonAncestor(root.right, left, right, result);
 		
 		if(findLeft != null && findRight != null) {
+			result = root;
 			return root;
 		}
 		
