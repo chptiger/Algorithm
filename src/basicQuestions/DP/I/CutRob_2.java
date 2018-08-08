4e91847808219ee7bb9cd882463acea45bbb23f0
xx65 <thomas.java863@gmail.com>
2018-08-08 08:28:18 -0700

    update

--- src/basicQuestions/DP/I/CutRob_2.java
+++ src/basicQuestions/DP/I/CutRob_2.java
@@ -10,17 +10,15 @@ public class CutRob_2 {
 		int n = 10;
 		Assert.assertEquals(36, cutRobDP(n)); // 3, 3 4
 	}
-	// 1 0
-	// 2 1    3 1,0 1; 2, 1 = 2
-	// 4 1 3
+//	O(n^2)
 	public static int cutRobDP(int n) {
 		if(n <= 1) {
 			return 0;
 		}
 		int[] M = new int[n + 1];
 		int global = 0;
-		for(int i = 2; i <= n; i++) { // the length of rob
-			for(int j = 1; j <= i/2; j++) {// how to cut the rob, from start to the middle, one by one
+		for(int i = 2; i <= n; i++) { // the length of rob, increasing by 1 meter every time
+			for(int j = 1; j <= i/2; j++) {// how to cut the rob, from start to the middle, one by one; 左大段的所有可能性
 				M[i] = Math.max(j, M[j]) * Math.max((i - j), M[i - j]);
 			}
 			global = Math.max(global, M[i]);
