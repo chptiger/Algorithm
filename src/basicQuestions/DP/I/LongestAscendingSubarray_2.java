19d77662674e0d50bf454e1bf456feb1537c0738
xx65 <thomas.java863@gmail.com>
2018-08-09 08:01:48 -0700

    udpate question descriptions

--- src/basicQuestions/DP/I/LongestAscendingSubarray_2.java
+++ src/basicQuestions/DP/I/LongestAscendingSubarray_2.java
@@ -3,7 +3,15 @@ package basicQuestions.DP.I;
 import org.junit.Assert;
 import org.junit.Test;
 
-
+/*
+Description
+Given an unsorted array, find the length of the longest subarray in which the numbers are in ascending order.
+Assumptions
+The given array is not null
+Examples
+{7, 2, 3, 1, 5, 8, 9, 6}, longest ascending subarray is {1, 5, 8, 9}, length is 4.
+{1, 2, 3, 3, 4, 4, 5}, longest ascending subarray is {1, 2, 3}, length is 3.
+*/
 public class LongestAscendingSubarray_2 {
 	
 	@Test
