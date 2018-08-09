b578889ab99e2198004e07122c35484507ac4574
chptiger <thomas.java863@gmail.com>
2018-08-08 20:38:31 -0700

    find longest ascending subarray -> DP & Recursive

---
+++ src/basicQuestions/DP/I/LongestAscendingSubarray_2.java
@@ -0,0 +1,53 @@
+package io.javabrains.unit1;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class LongestAscendingSubarray_2 {
+	
+	@Test
+	public void test_longestAscendingSubarray() {
+		int[] input = {7, 3, 2, 3, 5, 7, 9, 10, 7, 9};
+		Assert.assertEquals(6, findLongestAscendingSubarrayDP(input));
+		int[] result = new int[2];
+		Assert.assertEquals(6, findLongestAscendingSubarrayRecursive(input, 1, result));
+	}
+	
+	
+//	DP
+	private int findLongestAscendingSubarrayDP(int[] input) {
+		if(input.length <= 1) {
+			return 1;
+		}
+		
+		int count = 0, max = 0;
+		for(int i = 1; i < input.length; i++) {
+			if(input[i] > input[i - 1]) {
+				count++;
+			} else {
+				count = 1;
+			}
+			max = Math.max(count, max);
+		}
+		return max;
+	}
+	
+	
+//	Recursive
+	private int findLongestAscendingSubarrayRecursive(int[] input, int index, int[] result) {
+		if(input.length <= 1) {
+			return 1;
+		}
+		if(index == input.length) {
+			return result[1];
+		}
+		if(input[index] > input[index - 1]) {
+			result[0] = result[0] + 1;
+		} else {
+			result[0] = 1;
+		}
+		result[1] = Math.max(result[1], result[0]);
+		return findLongestAscendingSubarrayRecursive(input, index + 1, result);
+	}
+}
