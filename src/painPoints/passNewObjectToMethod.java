53ff697c6dd8a5bbce5a16e5162be45865ba6d05
xx65 <thomas.java863@gmail.com>
2018-08-15 08:34:31 -0700

    updated value only in the swap() not in reverse()

---
+++ src/painPoints/passNewObjectToMethod.java
@@ -0,0 +1,40 @@
+package painPoints;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * When I use the method input.toCharArray() to pass the char[] to the swap method.
+ * 
+ * The updated is only in the swap(), and could not get the updated input in the reverse().
+ * 
+ * Reason:
+ * A new character array was created by this method. 
+ * The updating is for the new character array, not for the original String. 
+ * 	toCharArray()
+ * 			Converts this string to a new character array.
+		    
+			Returns:
+			
+			a newly allocated character array whose length is the length of this string 
+			and whose contents are initialized to contain the character sequence represented by this string.
+ * @author xx65
+ *
+ */
+public class passNewObjectToMethod {
+	
+	@Test
+	public void test_reverseString() {
+		String input = "abc";
+		Assert.assertEquals("cba", reverse(input, 0, input.length() - 1));
+	}
+	
+	private static String reverse(String input, int start, int end) {
+		if(start >= end) {
+			return input;
+		}
+		
+		utilities.StringUtil.swap(input.toCharArray(), start, end);
+		return reverse(input, start + 1, end - 1);
+	}
+}
