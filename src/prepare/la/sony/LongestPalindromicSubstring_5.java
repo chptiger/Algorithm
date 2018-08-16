f75fc199d153dc370734c8075f1a64ad24816f81
chptiger <thomas.java863@gmail.com>
2018-08-15 20:56:19 -0700

    Update LongestPalindromicSubstring_5.java
    DP: Longest Palindromic Subsring

--- src/prepare/la/sony/LongestPalindromicSubstring_5.java
+++ src/prepare/la/sony/LongestPalindromicSubstring_5.java
@@ -21,7 +21,8 @@ Solution:
 	https://leetcode.com/problems/longest-palindromic-substring/solution/
 	http://www.cnblogs.com/grandyang/p/4464476.html
 	https://www.cnblogs.com/yuzhangcmu/p/4189068.html
-	
+
+
  * @author xx65
  *
  */
@@ -30,15 +31,39 @@ public class LongestPalindromicSubstring_5 {
 	@Test
 	public void test_longestPalindromicSubsring() {
 		String input = "babad";
-		Assert.assertEquals("bab", longestPalindromicSubsring(input.toCharArray()));
+		Assert.assertEquals("bab", longestPalindromicSubsringDP(input.toCharArray()));
 	}
-	
-	private static String longestPalindromicSubsring(char[] input) {
+	/**
+	此题还可以用动态规划Dynamic Programming来解，根Palindrome Partitioning II 拆分回文串之二的解法很类似，
+	我们维护一个二维数组dp，其中dp[i][j]表示字符串区间[i, j]是否为回文串，
+	当i = j时，只有一个字符，肯定是回文串，
+	如果i = j + 1，说明是相邻字符，此时需要判断s[i]是否等于s[j]，
+	如果i和j不相邻，即i - j >= 2时，除了判断s[i]和s[j]相等之外，dp[j + 1][i - 1]若为真，就是回文串，
+	通过以上分析，可以写出递推式如下：
+
+	dp[i, j] = 1                                     if i == j
+
+           = s[i] == s[j]                                if j = i + 1
+
+           = s[i] == s[j] && dp[i + 1][j - 1]            if j > i + 1   
+	**/
+	private static String longestPalindromicSubsringDP(char[] input) {
 		if(input == null) {
 			return null;
 		}
 		boolean[][] isP = new boolean[input.length][input.length];
-		String result = "";
-		
+		int max = 0, start = 0, end = 0;
+		for(int i = 0; i < input.length; i++) {
+			for(int j = i; j < input.length; j++) {
+				isP[i][j] = input[i] == input[j] && (input[i + 1] == input[j - 1] || j - i < 2)
+					if(isP[i][j] && j - i + 1 > max) {
+						max = j - i + 1;
+						start = i;
+						end = j;
+					}
+					
+			}
+		}
+		return new String(input).subString(i, j + 1);
 	}
 }
