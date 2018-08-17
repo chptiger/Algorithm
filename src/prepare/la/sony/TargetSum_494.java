52c8c50685b15f3c7cf23c895554ca22cc3b160f
xx65 <thomas.java863@gmail.com>
2018-08-17 08:08:02 -0700

    update

---
+++ src/prepare/la/sony/TargetSum_494.java
@@ -0,0 +1,43 @@
+package prepare.la.sony;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * You are given a list of non-negative integers, a1, a2, ..., an, and a target, S. Now you have 2 symbols + and -. 
+ * For each integer, you should choose one from + and - as its new symbol.
+
+	Find out how many ways to assign symbols to make sum of integers equal to target S.
+	
+	Example 1:
+	Input: nums is [1, 1, 1, 1, 1], S is 3. 
+	Output: 5
+	Explanation: 
+	
+	-1+1+1+1+1 = 3
+	+1-1+1+1+1 = 3
+	+1+1-1+1+1 = 3
+	+1+1+1-1+1 = 3
+	+1+1+1+1-1 = 3
+	
+	There are 5 ways to assign symbols to make the sum of nums be target 3.
+	Note:
+	The length of the given array is positive and will not exceed 20.
+	The sum of elements in the given array will not exceed 1000.
+	Your output answer is guaranteed to be fitted in a 32-bit integer.
+ * @author xx65
+ *
+ */
+public class TargetSum_494 {
+	
+	@Test
+	public void test_targetSum() {
+		int[] arr = {1, 1, 1, 1, 1};
+		Assert.assertEquals(5, targetSum(arr, 3));
+	}
+	
+	private static int targetSum(int[] arr, int target) {
+		
+		return 0;
+	}
+}
