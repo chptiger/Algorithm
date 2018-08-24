571379bd0f306cda6983a515d48881d43d8a64f7
xx65 <thomas.java863@gmail.com>
2018-08-24 09:45:31 -0700

    Target Sum 494: DFS(recursioon)

--- src/prepare/la/sony/TargetSum_494.java
+++ src/prepare/la/sony/TargetSum_494.java
@@ -29,15 +29,44 @@ import org.junit.Test;
  *
  */
 public class TargetSum_494 {
-	
+	static int count;
 	@Test
 	public void test_targetSum() {
 		int[] arr = {1, 1, 1, 1, 1};
-		Assert.assertEquals(5, targetSum(arr, 3));
+		Assert.assertEquals(5, targetSumDSF(arr, 3));
 	}
 	
-	private static int targetSum(int[] arr, int target) {
-		
-		return 0;
+	private static int targetSumDSF(int[] arr, int target) {
+		count(arr, 0, 0, target);
+		return count;
+	}
+	
+	/**
+	 	we should put count() into the else {}
+	 	1. if arr length is 1, there will be ArrayOutOfBoundsException for second count() method
+
+	 	   
+	 */
+	private static void count_wrong(int[] arr, int index, int sum, int target) {
+		if(index == arr.length) {
+			if(sum == target) { // if(index == arr.length && sum == target) we could not return in the final level
+				count++;
+			}
+			return;
+		} // we should put the follow codes into _lse {}  
+		count(arr, index + 1, sum + arr[index], target);
+		count(arr, index - 1, sum - arr[index], target); // it should index + 1 	 	2. if arr length larger than 1, there will be StackOverFlowError the method will alwarys run two count() methods.
+	}
+	
+	// if else or add return to make current method complete to avoid StackOverFlowError.
+	private static void count(int[] arr, int index, int sum, int target) {
+		if (index == arr.length) {
+			if (sum == target)
+				count++;
+			// return; // do not need return statement there. we use if else, the current count method was completed.
+		} else {
+			count(arr, index + 1, sum + arr[index], target);
+			count(arr, index + 1, sum - arr[index], target);
+		}
 	}
 }
